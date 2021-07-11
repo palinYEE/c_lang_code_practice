2f90efc68bc61c50ac75805beba7a694efe6c18a
yoon-yeoungjin <alwns28@kookmin.ac.kr>
2021-07-12 08:56:52 +0900

    start 6549

--- try/9376.escapePrison.c
+++ try/9376.escapePrison.c
@@ -1,11 +1,27 @@
 #include<stdio.h>
+const int MAX = 102;
+const int INF = 987654321;
 
-/*
- $ : 죄수
- . : 빈 공간
- * : 벽
- # : 문
-*/
+typedef struct 
+{
+    int y,x;
+}Dir;
+
+Dir moveDir[4] = {{1,0}, {-1, 0}, {0, 1}, {0, -1}};
+
+
+void printMatrix(char matrix[][100],int h, int w, int i){
+    int j, k;
+    printf("\n ====================\n");
+    printf("%d-th testCase matrix \n", i);
+    // Check matrix
+    for(j=0; j<h; j++){
+        for(k=0; k<w; k++){
+            printf("%c ", matrix[j][k]);
+        }
+        printf("\n");
+    }
+}
 int main(){
     int testCase, h, w;
     char prison[100][100] = {0,};
@@ -18,18 +34,9 @@ int main(){
         for(j=0; j<h; j++){
             scanf("%s", prison[j]);
         }
-
-        printf("\n ====================\n");
-        printf("%d-th testCase matrix \n", i);
-        // Check matrix
-        for(j=0; j<h; j++){
-            for(k=0; k<w; k++){
-                printf("%c ", prison[j][k]);
-            }
-            printf("\n");
-        }
         
-        
+        // prison check complete.
+        // printMatrix(prison, h, w, i);
     }
 
     
