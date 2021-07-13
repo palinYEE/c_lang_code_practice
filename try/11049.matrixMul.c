ccdc1403d5e55bc39a8563f87696ccf222134fb3
yoon-yeoungjin <alwns28@kookmin.ac.kr>
2021-07-13 14:40:21 +0900

    11049

---
+++ try/11049.matrixMul.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/*
+ - 행렬 곱셈 조건 : 앞의 행렬의 열 개수 == 뒤의 행렬의 행 개수
+ - 접근 방식
+    - Floyd-Warshall Algorithm과 비슷하게...?
+    - 출처 : https://justicehui.github.io/ps/2018/10/28/BOJ11049/
+*/
+int main(){
+    int N;
+
+    return 0;
+}
